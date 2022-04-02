bb856c6e8c283dee318b2c374c611dedee0331d9
MagaliGiansiracusa <giansiracusamagali@gmail.com>
2022-04-02 11:31:03 -0300

    cambios

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require("express");
-const app = express;
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3001;
+const mainRouter = require("./routers/main");
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
-    });
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/", mainRouter);
+
+app.listen(PORT, ()=>{
+    console.log("Server running ${PORT}");
+});
+
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname + "/views/home.html")
+})
+
+app.get("/about",(req,res)=>{
+    res.sendFile(__dirname + "/views/about.html")
+})
+
